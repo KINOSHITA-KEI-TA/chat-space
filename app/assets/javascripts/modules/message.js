8f12c3be708e1e3549d4ba6d9b78a396d716cf96
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-07-22 22:14:21 +0900

    auto-update

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,8 +1,9 @@
 $(function(){
   function buildHTML(message){
-    if (message.image) {
+   
+    if ( message.image ) {
       let html = 
-        `<div class="Chat-main__message-list__message-box">
+      `<div class="Chat-main__message-list__message-box" data-message-id=${message.id}>
           <div class="Chat-main__message-list__message-box__message-name">
             <div class="Chat-main__message-list__message-box__message-name__user-name">
             ${message.user_name}
@@ -18,30 +19,30 @@ $(function(){
             <img class="Message__image" src="${message.image}">
           </div>
         </div>`
+        return html;
     } else {
       let html = 
-      `
-          <div class="Chat-main__message-list__message-box">
-          <div class="Chat-main__message-list__message-box__message-name">
-            <div class="Chat-main__message-list__message-box__message-name__user-name">
+      `<div class="Chat-main__message-list__message-box" data-message-id=${message.id}>
+        <div class="Chat-main__message-list__message-box__message-name">
+          <div class="Chat-main__message-list__message-box__message-name__user-name">
             ${message.user_name}
-            </div>
-            <div class="Chat-main__message-list__message-box__message-name__time">
-            ${message.created_at}
-            </div>
           </div>
+          <div class="Chat-main__message-list__message-box__message-name__time">
+            ${message.created_at}
+          </div>
+        </div>
           <div class="Chat-main__message-list__message-box__message-word">
             <p class="Message__content">
             ${message.text}
             </p>
-          
           </div>
-     `
+      </div>`
     return html;
     };
   }
-  $('.Form-box').on('submit',function(e){
-    e.preventDefault()
+
+  $('.Form-box').on('submit', function(e){
+    e.preventDefault();
     
     let formData = new FormData(this);
     let url = $(this).attr('action')
@@ -61,7 +62,8 @@ $(function(){
       $('.Form-box__submitBtn').prop('disabled', false);
     })
     .fail(function(){
-      alert("エラーです")
+      alert("エラーです");
+      $('.Form-box__submitBtn').prop("disabled", false);
     });
-  })
+  });
 });
